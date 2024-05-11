0a1e8fa6457136857d323e8c12e1dd26505fe284
Yabiti <yabetseyohannes@gmail.com>
2024-05-11 13:05:19 +0300

    .

--- home.py
+++ home.py
@@ -5,3 +5,7 @@ def __int__(self, name, house):
 def main():
     student = get_student()
     print(f"{student.name} from {student.house}")
+
+def get_student():
+    name = input("name: ")
+    house = input("house: ")
